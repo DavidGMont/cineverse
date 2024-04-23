825cbe08f20a6e2be82d9f5f5991c0d1c30e7b0f
DavidGMont <davidgarmo@outlook.com>
2024-04-23 13:03:57 -0500

    🐳 Auto loadable data added and Config Server adjusts performed

---
+++ movie-service/src/main/resources/data.sql
@@ -0,0 +1,78 @@
+USE movies;
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('Inception', 'Action', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('The Shawshank Redemption', 'Drama', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('The Godfather', 'Crime', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('Pulp Fiction', 'Thriller', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('The Dark Knight', 'Action', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('Forrest Gump', 'Drama', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('Fight Club', 'Thriller', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('The Matrix', 'Action', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('Goodfellas', 'Crime', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('The Lord of the Rings: The Fellowship of the Ring', 'Fantasy',
+        CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('The Silence of the Lambs', 'Horror', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('The Green Mile', 'Drama', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('The Godfather: Part II', 'Crime', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('Se7en', 'Thriller', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('The Lord of the Rings: The Return of the King', 'Fantasy', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('Gladiator', 'Action', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('The Departed', 'Crime', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('Inglourious Basterds', 'War', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('The Lord of the Rings: The Two Towers', 'Fantasy', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('The Lion King', 'Animation', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('Saving Private Ryan', 'War', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('The Pianist', 'Biography', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('The Shining', 'Horror', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('The Prestige', 'Drama', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('American History X', 'Drama', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('Interstellar', 'Adventure', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('The Usual Suspects', 'Crime', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('Leon: The Professional', 'Crime', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('Terminator 2: Judgment Day', 'Action', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('The Sixth Sense', 'Thriller', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('Back to the Future', 'Adventure', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('The Intouchables', 'Comedy', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('The Gladiator', 'Action', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('Schindler\'s List', 'Biography', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('The Godfather: Part III', 'Crime', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('Whiplash', 'Drama', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('The Dark Knight Rises', 'Action', CONCAT('https://www.cineverse.com/movie/', UUID()));
+INSERT INTO movie(title, genre, streaming_url)
+VALUES ('The Good, the Bad and the Ugly', 'Western', CONCAT('https://www.cineverse.com/movie/', UUID()));

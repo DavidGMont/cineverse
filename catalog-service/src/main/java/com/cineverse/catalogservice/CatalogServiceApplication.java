dd52d5c2f454597fd86ca58f7f4ef977cbd0df17
DavidGMont <davidgarmo@outlook.com>
2024-04-23 18:33:08 -0500

    🌐 Feign Client enabled

--- catalog-service/src/main/java/com/cineverse/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/cineverse/catalogservice/CatalogServiceApplication.java
@@ -1,13 +1,16 @@
 package com.cineverse.catalogservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@Slf4j
 public class CatalogServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CatalogServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogServiceApplication.class, args);
+        LOGGER.info("📚 Catalog Service started successfully");
+    }
 }

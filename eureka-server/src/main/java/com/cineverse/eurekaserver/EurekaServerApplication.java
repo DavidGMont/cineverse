1025268d1e0b060125f745b42ff4b235c5787e22
DavidGMont <davidgarmo@outlook.com>
2024-04-23 00:58:43 -0500

    ✨ Eureka Server YAML and annotations added

--- eureka-server/src/main/java/com/cineverse/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/cineverse/eurekaserver/EurekaServerApplication.java
@@ -1,13 +1,20 @@
 package com.cineverse.eurekaserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
+@RefreshScope
 public class EurekaServerApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EurekaServerApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+        LOGGER.info("📓 Eureka Server started successfully");
+    }
 }
